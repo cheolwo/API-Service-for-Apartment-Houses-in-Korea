940345f8d8767e0ac061757c23768c647bd3026b
cheolwo <powerqkrcjfd@gmail.com>
2024-05-01 18:25:38 +0900

    .

--- 국토교통부 공공데이터/주거(司空)수집서버/obj/Debug/net8.0/주거(司空)수집서버.AssemblyInfo.cs
+++ 국토교통부 공공데이터/주거(司空)수집서버/obj/Debug/net8.0/주거(司空)수집서버.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("주거(司空)수집서버")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ae71bea8e37c45991cc053f58003440c6aa546")]
 [assembly: System.Reflection.AssemblyProductAttribute("주거(司空)수집서버")]
 [assembly: System.Reflection.AssemblyTitleAttribute("주거(司空)수집서버")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
